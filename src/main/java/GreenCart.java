79167a1472ccbc8fb39775655e124242d947afc4
Vladyslav <vladyslav1819@gmail.com>
2023-09-16 12:35:48 +0300

    green cart practice

--- src/main/java/GreenCart.java
+++ src/main/java/GreenCart.java
@@ -3,33 +3,54 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class GreenCart {
     public  static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        int j = 0;
+        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
+
         String[] vaggies = {"Cucumber", "Brocolli", "Beetroot"};
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
         Thread.sleep(3000);
+        addItems(driver, vaggies);
+        driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+        driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.className("promoCode")));
+        driver.findElement(By.className("promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("button.promoBtn")).click();
+
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+        System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+
+    }
+
+    public static void addItems(WebDriver driver, String[] vaggies){
+        int j = 0;
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 
+
         for(int i = 0; i < products.size(); i++){
-           String[] name = products.get(i).getText().split("-");
-           String formatedNames = name[0].trim();
+            String[] name = products.get(i).getText().split("-");
+            String formatedNames = name[0].trim();
 
-           List itemsNeededList = Arrays.asList(vaggies);
+            List itemsNeededList = Arrays.asList(vaggies);
 
 
-           if(itemsNeededList.contains(formatedNames)){
-               j++;
-               driver.findElement(By.xpath("//button[text()='ADD TO CART']")).click();
+            if(itemsNeededList.contains(formatedNames)){
+                j++;
+                driver.findElement(By.xpath("//button[text()='ADD TO CART']")).click();
                 if(j == 3){
                     break;
                 }
-           }
+            }
         }
-
     }
 }
